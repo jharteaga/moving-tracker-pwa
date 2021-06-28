f83a5ca2e475935b058efc02cc7c302ef2a912ed
Alejandra Hernandez <ehernandezvega00@mylangara.ca>
2021-06-28 18:46:11 -0500

    tested box class

--- js/box.js
+++ js/box.js
@@ -1,80 +1,77 @@
 
+
 class Box{
-    constructor(idMoving, idBox="", name="", description="", label="", boxSize="", weight="", fragile="", status=""){
-        this.idMoving = idMoving;
-        this.idBox=idBox;
-        this.name = name;
-        this.description = description;
-        this.label = label;
-        this.boxSize = boxSize;
-        this.weight = weight;
-        this.fragile = fragile;
-        this.status = status;
+
+    constructor(){
+        this.idMoving = "";
+        this.idBox="";
+        this.name = "";
+        this.description = "";
+        this.label = "";
+        this.boxSize = "";
+        this.weight = "";
+        this.fragile = "";
+        this.status = "";
     }
 
-    add(){
-        db.collection(`/movings/${this.idMoving}/boxes`).doc(this.idBox).set({
-            idMoving: this.idMoving,
-            name: this.name,
-            description: this.description,
-            label: this.label,
-            boxSize: this.boxSize,
-            weight: this.weight,
-            fragile: this.fragile,
-            status: this.status
+    add(idMoving, idBox="", name="", description="", label="", boxSize="", weight="", fragile=0, status="open"){
+        db.collection(`/movings/${idMoving}/boxes`).add({
+            idMoving: idMoving,
+            name: name,
+            description: description,
+            label: label,
+            boxSize: boxSize,
+            weight: weight,
+            fragile: fragile,
+            status: status
         })
-        .then(() => {
-            // console.log("Document successfully written!");
+        .then((doc_ref ) => {
             return "Document successfully written!";
         })
         .catch((error) => {
-            // console.error("Error writing document: ", error);
             return "Error writing document: ", error;
         });
     }
-
  
-    delete(idBox=this.idBox){
-        db.collection(`/movings/${this.idMoving}/boxes`).doc(idBox).delete().then(() => {
-            // console.log("Document successfully deleted!");
+    delete(idMoving,idBox){
+        db.collection(`/movings/${idMoving}/boxes`).doc(idBox).delete().then(() => {
             return "Document successfully deleted!";
         }).catch((error) => {
-            // console.error("Error removing document: ", error);
             return "Error removing document: ", error;
         });
     }
-    update (){
-        let box = db.collection(`/movings/${this.idMoving}/boxes`).doc(this.idBoxame);
 
-        // Set the "capital" field of the city 'DC'
+    update (idMoving, idBox, name, description, label, boxSize, weight, fragile, status){
+        let box = db.collection(`/movings/${idMoving}/boxes`).doc(idBox);
+
         return box.update({
-            name: this.name,
-            description: this.description,
-            label: this.label,
-            boxSize: this.boxSize,
-            weight: this.weight,
-            fragile: this.fragile,
-            status: this.status
+            name: name,
+            description: description,
+            label: label,
+            boxSize: boxSize,
+            weight: weight,
+            fragile: fragile,
+            status: status
         })
         .then(() => {
-            // console.log("Document successfully updated!");
             return "Document successfully updated!"
         })
         .catch((error) => {
-            // The document probably doesn't exist.
-            // console.error("Error updating document: ", error);
             return "Error updating document: ", error;
         });
     }
 
-    getBox(){
-
-        let boxDocument = db.collection(`/movings/${this.idMoving}/boxes`).doc(this.idBox);
+    getBox(idMoving,idBox){
+        let boxDocument = db.collection(`/movings/${idMoving}/boxes`).doc(idBox);
         let box = [];
+        
         boxDocument.get().then((doc) => {
             if (doc.exists) {
+                // console.log(`${doc. id} => ${doc. data()}`)              
+                const id = `${doc. id}`
                 box.push (
                     {
+                        id: id,
                         name: doc.data().name,
                         description: doc.data().description,
                         label: doc.data().label,
@@ -84,38 +81,40 @@ class Box{
                         status: doc.data().status
                     }
                 );
+                // console.log(box);
                 return box;
-                // console.log("Document data:", doc.data());
+                
             } else {
-                // doc.data() will be undefined in this case
-                return "No such document!";
+                // console.log("No such document!")
+                return "Document not found!";
             }
         }).catch((error) => {
             return `Error getting document: ${error}`;
         });
     }
 
-    getItems(){
+    getItems(idMoving,idBox){
         
-       
-        let items = db.collection(`/movings/${this.idMoving}/boxes/${this.idBox}/items`);
+        let items = db.collection(`/movings/${idMoving}/boxes/${idBox}/items`);
 
         let itemsDocs= [];
 
         items.get().then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
-                // doc.data() is never undefined for query doc snapshots
-                itemsDocs.push({idItem: doc.id, name:doc.data().name,
+                    let id = `${doc. id}`
+                    itemsDocs.push({
+                    id:id,    
+                    name:doc.data().name,
                     description: doc.data().description,
-                    athegory: doc.data().cathegory,
+                    cathegory: doc.data().cathegory,
                     quantity: doc.data().quantity,
                     value: doc.data().value 
                     })
-                // console.log(doc.id, " => ", doc.data());
             });
         });
-
         return itemsDocs;
     }
     
 }
+
+
