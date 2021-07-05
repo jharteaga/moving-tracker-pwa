9416a63d1323006a03a3980f7b89a1a6357d0405
Megumi Takashima <mtakashima00@mylangara.ca>
2021-07-05 09:00:20 -0700

    Connect sign up to user class

--- js/sign-up.js
+++ js/sign-up.js
@@ -2,29 +2,29 @@ const signUpBtn = document.getElementById("signUpBtn");
 const pwcredential = document.getElementById("pwcredential");
 
 //User List
-let users = [
-    {
-        userId: 1,
-        userEmail: "mtakashima00@mylangara.ca",
-        userName: "Meg",
-        password: "Abcdefg1"
-    },
-    {
-        userId: 2,
-        userEmail: "ehernandezvega00@mylangara.ca",
-        userName: "Alejandra",
-        password: "Qwertyu1"
-    }
-]; 
+// let users = [
+//     {
+//         userId: 1,
+//         userEmail: "mtakashima00@mylangara.ca",
+//         userName: "Meg",
+//         password: "Abcdefg1"
+//     },
+//     {
+//         userId: 2,
+//         userEmail: "ehernandezvega00@mylangara.ca",
+//         userName: "Alejandra",
+//         password: "Qwertyu1"
+//     }
+// ]; 
 
-class User {
-    constructor (userId, userEmail,userName, password) {
-        this.userId = userId;
-        this.Email = userEmail;
-        this.userName = userName;
-        this.password = password;
-    }
-}
+// class User {
+//     constructor (userId, userEmail,userName, password) {
+//         this.userId = userId;
+//         this.Email = userEmail;
+//         this.userName = userName;
+//         this.password = password;
+//     }
+// }
 
 pwcredential.addEventListener('click', () => {
     alert("Minimum 6 letters");
@@ -53,51 +53,53 @@ signUpBtn.addEventListener('click', () => {
 
 
 
-    let userExist = 0;
-    for(i=0; i < users.length; i++){
-        if(userNameInput === users[i].userName){
-            userExist = 1;
-        }    
-    }
+    // let userExist = 0;
+    // for(i=0; i < users.length; i++){
+    //     if(userNameInput === users[i].userName){
+    //         userExist = 1;
+    //     }    
+    // }
 
     // console.log(userExist);
 
-    if(emailInput === "" || emailInput.match(/@/) === null){
-        emailErrorMsg.innerHTML = "Please enter Email Address"; 
+    // if(emailInput === "" || emailInput.match(/@/) === null){
+    //     emailErrorMsg.innerHTML = "Please enter Email Address"; 
 
-    } else if(userNameInput === ""){
-        userNameErrorMsg.innerHTML = "Please enter User Name"; 
+    // } else if(userNameInput === ""){
+    //     userNameErrorMsg.innerHTML = "Please enter User Name"; 
 
-    } else if (userExist === 1){
-        userNameErrorMsg.innerHTML = `${userNameInput} is taken`; 
+    // } else if (userExist === 1){
+    //     userNameErrorMsg.innerHTML = `${userNameInput} is taken`; 
 
-    } else if(pwInput === ""){
-        pwErrorMsg.innerHTML = "Please enter Password"; 
+    // } else if(pwInput === ""){
+    //     pwErrorMsg.innerHTML = "Please enter Password"; 
 
-    } else if (pwInput.length < 6){
-        pwErrorMsg.innerHTML = "Shuold be at least 6 letters"; 
+    // } else if (pwInput.length < 6){
+    //     pwErrorMsg.innerHTML = "Shuold be at least 6 letters"; 
 
-    } else if(confirmPwInput === ""){
-        confirmPwErrorMsg.innerHTML = "Please confirm password"; 
+    // } else if(confirmPwInput === ""){
+    //     confirmPwErrorMsg.innerHTML = "Please confirm password"; 
 
-    } else if (confirmPwInput !== pwInput){
-        confirmPwErrorMsg.innerHTML = "Password doesn't match"; 
+    // } else if (confirmPwInput !== pwInput){
+    //     confirmPwErrorMsg.innerHTML = "Password doesn't match"; 
 
-    } else {
+    // } else {
         
-        const lastId = users[users.length -1].userId;
-        const userId = lastId + 1;
+    //     const lastId = users[users.length -1].userId;
+    //     const userId = lastId + 1;
 
-        const userToPush = new User (userId, emailInput, userNameInput, pwInput);
-        // console.log(userToPush);
+    //     const userToPush = new User (userId, emailInput, userNameInput, pwInput);
+    //     // console.log(userToPush);
 
-        users.push(userToPush);
-        console.log(users);
+    //     users.push(userToPush);
+    //     console.log(users);
 
-        window.location.href = "../pages/welcome.html";
-    }
+    //     window.location.href = "../pages/welcome.html";
+    // }
+
+    userToPush.userSignUp(emailInput, userNameInput, pwInput, confirmPwInput);
 })
 
-
+const userToPush = new User();
 
 
