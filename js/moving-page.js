2e513b6d7e85b567e90d6636058612a1c4cbd2f6
Sam Villegas <svillegaswarre00@mylangara.ca>
2021-07-13 18:41:48 -0700

    movings page connected, create and edit moving

---
+++ js/moving-page.js
@@ -0,0 +1,124 @@
+/**
+ * Movings Options Menu (Toggle plus menu button)
+ */
+const menuBtn = document.querySelector('.moving-details .options-btn');
+const movingOptions = document.querySelector('.moving-details .moving-options');
+
+menuBtn.addEventListener('click', () => {
+    movingOptions.classList.toggle('active');
+});
+
+const movingsOutput = document.querySelector('.movings-wrapper');
+const movingsArr = [];
+
+function renderMovings() {
+    movingsOutput.innerHTML = '';
+    movingsArr.forEach((moving) => {
+        console.log('dsfsdffd');
+        const movingWrapper = document.createElement('div');
+        movingWrapper.classList.add('moving-wrapper');
+        movingWrapper.id = moving.id;
+        const movingTitleDiv = document.createElement('div');
+        movingTitleDiv.classList.add('moving__title');
+        movingTitleDiv.innerHTML = `<h3>Moving To</h3><h4>${
+            moving.data().movingTitle
+        }</h4>`;
+        const movingAddressDiv = document.createElement('div');
+        movingAddressDiv.classList.add('moving__address');
+        movingAddressDiv.innerHTML = `<p>${moving.data().to}</p>`;
+        const movingDateDiv = document.createElement('div');
+        movingDateDiv.classList.add('moving__date');
+        movingDateDiv.innerHTML = `${moving.data().date}`;
+        const movingActions = document.createElement('div');
+        movingActions.classList.add('moving__actions');
+        movingActions.innerHTML = `
+                        <button class="icon button-edit" data-bs-toggle="modal" data-bs-target="#editMovingModal" id="edit-${moving.id}">
+                            <span class="fas fa-pencil-alt"></span>
+                        </button>
+                        <button class="icon button-delete">
+                            <span class="fas fa-trash"></span>
+                        </button>
+                    `;
+        movingWrapper.appendChild(movingTitleDiv);
+        movingWrapper.appendChild(movingAddressDiv);
+        movingWrapper.appendChild(movingDateDiv);
+        movingWrapper.appendChild(movingActions);
+        movingsOutput.appendChild(movingWrapper);
+    });
+}
+
+// Save Moving Section
+const saveMovingBtn = document.querySelector('#createMovingForm');
+
+saveMovingBtn.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const movingTitle = document.querySelector('#movingTitle').value;
+    const movingDescription =
+        document.querySelector('#movingDescription').value;
+    const movingFrom = document.querySelector('#movingFrom').value;
+    const movingTo = document.querySelector('#movingTo').value;
+    const movingDate = document.querySelector('#movingDate').value;
+    const moving = new Moving(
+        user.userId,
+        movingTitle,
+        movingDescription,
+        movingFrom,
+        movingTo,
+        movingDate
+    );
+    await moving.addMovingToDb(user);
+});
+
+// End Save Moving Section
+
+// Edit Moving Section
+
+const editMovingTitle = document.querySelector('#editMovingTitle');
+const editMovingDescription = document.querySelector('#editMovingDescription');
+const editMovingFrom = document.querySelector('#editMovingFrom');
+const editMovingTo = document.querySelector('#editMovingTo');
+const editMovingDate = document.querySelector('#editMovingDate');
+
+// const saveEditMovingBtn = document.querySelector('#saveEditMovingBtn');
+const saveEditMovingBtn = document.querySelector('#editMovingForm');
+
+saveEditMovingBtn.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    console.log(editMovingTitle.value);
+    await moving.updateMoving(
+        user,
+        editMovingTitle.value,
+        editMovingDescription.value,
+        editMovingFrom.value,
+        editMovingTo.value,
+        editMovingDate.value
+    );
+});
+
+const moving = new Moving();
+function addEventListersEdit() {
+    const editButtons = document.querySelectorAll('.button-edit');
+    editButtons.forEach((button) => {
+        const movingId = button.id.split('-')[1];
+        const elem = document.querySelector(`#${button.id}`);
+        elem.addEventListener('click', async (event) => {
+            editMovingTitle.value = '';
+            editMovingDescription.value = '';
+            editMovingFrom.value = '';
+            editMovingTo.value = '';
+            editMovingDate.value = '';
+            event.preventDefault();
+            await moving.getMovingById(movingId);
+            console.log(moving);
+            editMovingTitle.value = moving.movingTitle;
+            editMovingDescription.value = moving.description;
+            editMovingFrom.value = moving.from;
+            editMovingTo.value = moving.to;
+            editMovingDate.value = moving.date;
+            console.log(moving.date);
+        });
+        // console.log(button.id);
+    });
+}
+// End Edit Moving Section
