32cd989154ec0e6c239d1e099008d1214253bf45
Sam Villegas <svillegaswarre00@mylangara.ca>
2021-07-22 14:33:46 -0700

    splash screen created

--- js/index.js
+++ js/index.js
@@ -1,8 +1,21 @@
-// setup materialize components
-document.addEventListener('DOMContentLoaded', function () {
-	var modals = document.querySelectorAll('.modal');
-	M.Modal.init(modals);
+const filter = document.querySelectorAll('feGaussianBlur');
+const logo = document.querySelector('.logo-wrapper');
 
-	var items = document.querySelectorAll('.collapsible');
-	M.Collapsible.init(items);
-});
+setTimeout(() => {
+  logo.style.opacity = 1;
+}, 2000);
+setTimeout(() => {
+  filter.forEach((e, ind) => {
+    for (let j = 8; j >= 0; j--) {
+      console.log(Math.floor(e.stdDeviationY.baseVal));
+      setTimeout(() => {
+        e.stdDeviationY.baseVal = j;
+        e.stdDeviationX.baseVal = j;
+      }, 50 * ind);
+    }
+  });
+}, 3000);
+
+setTimeout(() => {
+  window.location.href = 'pages/sign-in.html';
+}, 3700);
