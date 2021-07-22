26bb5f1da41da2602ababec90a0448906dd859b7
Sam Villegas <svillegaswarre00@mylangara.ca>
2021-07-22 10:34:06 -0700

    connected to firebase storage and created method for user to upload profile picture

--- js/firebase.js
+++ js/firebase.js
@@ -1,13 +1,15 @@
 const firebaseConfig = {
-    apiKey: 'AIzaSyCwac9_2O7Xnsroyi_FAmSQQTjdk7Y2vrI',
-    authDomain: 'moving-tracker-pwa.firebaseapp.com',
-    projectId: 'moving-tracker-pwa',
-    storageBucket: 'moving-tracker-pwa.appspot.com',
-    messagingSenderId: '680571252158',
-    appId: '1:680571252158:web:0f2938caa0d5d1ff5e1def',
+  apiKey: 'AIzaSyCwac9_2O7Xnsroyi_FAmSQQTjdk7Y2vrI',
+  authDomain: 'moving-tracker-pwa.firebaseapp.com',
+  projectId: 'moving-tracker-pwa',
+  storageBucket: 'moving-tracker-pwa.appspot.com',
+  messagingSenderId: '680571252158',
+  appId: '1:680571252158:web:0f2938caa0d5d1ff5e1def',
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage().ref();
+console.log(storage);
--- js/user-profile.js
+++ js/user-profile.js
@@ -1,119 +1,97 @@
-console.log("connected");
-
+console.log('connected');
 
 //Define
 const user = new User();
-const userName = document.getElementById("userName");
-const userEmail = document.getElementById("userEmail");
-const newUserName = document.getElementById("newUserName");
+const userName = document.getElementById('userName');
+const userEmail = document.getElementById('userEmail');
+const newUserName = document.getElementById('newUserName');
 
-const editUserPicBtn = document.getElementById("editUserPicBtn");
-const closeBtn = document.getElementById("closeBtn");
-const picEditMethod = document.querySelector(".picEditMethod");
+const editUserPicBtn = document.getElementById('editUserPicBtn');
+const closeBtn = document.getElementById('closeBtn');
+const picEditMethod = document.querySelector('.picEditMethod');
 
-const editUserNameBtn = document.getElementById("editUserNameBtn");
+const editUserNameBtn = document.getElementById('editUserNameBtn');
 const closeEditUserNameBtn = document.getElementById('closeEditUserNameBtn');
-const profileName = document.getElementById("profile-name");
-const logoutBtn = document.getElementById("logoutBtn");
+const profileName = document.getElementById('profile-name');
+const logoutBtn = document.getElementById('logoutBtn');
 
-const nameSaveBtn = document.getElementById("nameSaveBtn");
+const nameSaveBtn = document.getElementById('nameSaveBtn');
 
-const fromPc = document.getElementById("fromPc");
-const fileSelector = document.getElementById("fileSelector");
-const picSaveBtn = document.getElementById("picSaveBtn");
-const fileSelctCloseBtn = document.getElementById("fileSelctCloseBtn");
-
+const fromPc = document.getElementById('fromPc');
+const fileSelector = document.getElementById('fileSelector');
+const picSaveBtn = document.getElementById('picSaveBtn');
+const fileSelctCloseBtn = document.getElementById('fileSelctCloseBtn');
 
 //When loading the page
-user.isLoggedIn(()=>{
-    userName.innerHTML = user.userName;
-    userEmail.innerHTML = user.email;
-    newUserName.value = user.userName;
+user.isLoggedIn(() => {
+  userName.innerHTML = user.userName;
+  userEmail.innerHTML = user.email;
+  newUserName.value = user.userName;
 });
-
-
 
 //Edit Picture
 editUserPicBtn.addEventListener('click', () => {
-    
-    picEditMethod.classList.remove("hidden");
-    logoutBtn.style.display = "none";
-
-})
+  picEditMethod.classList.remove('hidden');
+  logoutBtn.style.display = 'none';
+});
 
 closeBtn.addEventListener('click', () => {
-
-    picEditMethod.classList.add("hidden");
-    logoutBtn.style.display = "flex";
-})
-
+  picEditMethod.classList.add('hidden');
+  logoutBtn.style.display = 'flex';
+});
 
 //Edit User Name
 editUserNameBtn.addEventListener('click', () => {
-    
-    profileName.classList.add("edit-mode");
-    logoutBtn.style.display = "none";
-})
+  profileName.classList.add('edit-mode');
+  logoutBtn.style.display = 'none';
+});
 
 closeEditUserNameBtn.addEventListener('click', () => {
-    
-    profileName.classList.remove("edit-mode");
-    logoutBtn.style.display = "flex";
-})
-
-
+  profileName.classList.remove('edit-mode');
+  logoutBtn.style.display = 'flex';
+});
 
 //Save New User Name
 nameSaveBtn.addEventListener('click', () => {
-    
-    //Update User Name
-    user.updateUserDb(newUserName.value);
+  //Update User Name
+  user.updateUserDb(newUserName.value);
 
-    //Close Edit Mode
-    profileName.classList.remove("edit-mode");
+  //Close Edit Mode
+  profileName.classList.remove('edit-mode');
 
-    //Reflect New User Name
-    user.isLoggedIn(()=>{
-        userName.innerHTML = user.userName;
-        newUserName.value = user.userName;
-    });
-})
+  //Reflect New User Name
+  user.isLoggedIn(() => {
+    userName.innerHTML = user.userName;
+    newUserName.value = user.userName;
+  });
+});
 
-
-
-//Logout 
+//Logout
 logoutBtn.addEventListener('click', () => {
-    //Logout
-    user.userLogout();
-    
-    //Send to login page
-    window.location.href = 'sign-in.html';
-})
+  //Logout
+  user.userLogout();
 
+  //Send to login page
+  window.location.href = 'sign-in.html';
+});
 
 //Choose from PC
 fromPc.addEventListener('click', () => {
-    fileSelector.style.display = "block";
-})
+  fileSelector.style.display = 'block';
+});
 
-
-//Choose from PC => Close 
+//Choose from PC => Close
 fileSelctCloseBtn.addEventListener('click', () => {
-    fileSelector.style.display = "none";
-    picEditMethod.classList.add("hidden");
-    logoutBtn.style.display = "flex";
-})
-
+  fileSelector.style.display = 'none';
+  picEditMethod.classList.add('hidden');
+  logoutBtn.style.display = 'flex';
+});
 
 //Choose from PC => Save
 picSaveBtn.addEventListener('click', () => {
-    fileSelector.style.display = "none";
-    picEditMethod.classList.add("hidden");
-    logoutBtn.style.display = "flex";
+  fileSelector.style.display = 'none';
+  picEditMethod.classList.add('hidden');
+  logoutBtn.style.display = 'flex';
 
-    //Need to add the code to send the file to Firestore
-
-
-    
-})
-
+  //Need to add the code to send the file to Firestore
+});
