a7dce3e437f255a9ffdf8cb812d98c17ac7bdbb1
Jose Arteaga <jarteaga00@mylangara.ca>
2021-07-27 00:11:18 -0700

    Add high-fi design to not-network page

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'v1';
+const cacheName = 'v1.1';
 const assetsToCache = [
   '/',
   '/index.html',
@@ -11,17 +11,10 @@ const assetsToCache = [
   '/img/moving-tracker-logo.svg',
   '/img/moving-tracker-icon.svg',
   '/img/drop-glow.svg',
-  '/img/onboarding/onboarding1.jpg',
-  '/img/onboarding/onboarding2.jpg',
-  '/img/onboarding/onboarding3.jpg',
-  '/img/onboarding/onboarding4.jpeg',
-  '/img/profile/user-default.png',
-  '/img/profile/user-default.svg',
-  '/img/profile/user-default.webp',
 ];
 
 self.addEventListener('install', (event) => {
-  console.log('[Service Worker] Installing Service Worker ...', event);
+  // console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
     // waitUntil tells the browser to wait for this to finish
     caches
@@ -34,11 +27,11 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('[Service Worker] Activating Service Worker ....', event);
+  // console.log('[Service Worker] Activating Service Worker ....', event);
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log(`Fetching ${event.request.url}`);
+  // console.log(`Fetching ${event.request.url}`);
   event.respondWith(
     (async () => {
       const response = await caches.match(event.request);
